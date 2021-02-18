31ee80bfaf2d321c24507e2f5fb8bb99a73e829b
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-02-18 17:31:05 -0600

    404 Handler Added

--- App.js
+++ App.js
@@ -10,6 +10,18 @@ const projectRoutes = require('./routes/project');
 app.use(mainRoutes);
 app.use('/project', projectRoutes);
 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
